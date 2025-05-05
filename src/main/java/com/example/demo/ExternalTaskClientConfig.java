dece7d4fb097e814c456cfa08ee355267a6e0e78
MoritzSchwarz <moritz.schwarz148@gmx.de>
2025-05-05 10:49:02 +0200

    GMT ready - adjustments to make runs better comparable

--- src/main/java/com/example/demo/ExternalTaskClientConfig.java
+++ src/main/java/com/example/demo/ExternalTaskClientConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import jakarta.annotation.PostConstruct;
 import org.camunda.bpm.client.ExternalTaskClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -12,7 +11,7 @@ public class ExternalTaskClientConfig {
     public ExternalTaskClient externalTaskClient(){
         return ExternalTaskClient.create()
 //                .baseUrl("http://localhost:8080/engine-rest")
-                .baseUrl("http://camundaEngine:8080/engine-rest")
+                .baseUrl("http://engine:8080/engine-rest")
                 .asyncResponseTimeout(5000)
                 .build();
     }
